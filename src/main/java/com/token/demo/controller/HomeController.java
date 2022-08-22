c60c10ea68d489011acd0c0c8850f89a7fddc616
unknown <cogjava3121@iiht.tech>
2022-08-22 09:37:20 +0530

    generated jwt token

--- src/main/java/com/token/demo/controller/HomeController.java
+++ src/main/java/com/token/demo/controller/HomeController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -39,6 +40,10 @@ public class HomeController {
 		} catch (BadCredentialsException e) {
 			throw new Exception("INVALID_CREDENTIALS", e);
 		}
+		
+		final UserDetails userDetails=userService.loadUserByUsername(jwtRequest.getUsername());
+		final String token=jwtUtility.generateToken(userDetails);
+		return new JwtResponse(token);
 	}
 }
 
